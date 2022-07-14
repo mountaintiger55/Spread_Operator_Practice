61d820681b1f7125b0e0f07bfae33e77ff4c676d
mountaintiger55 <99082587+mountaintiger55@users.noreply.github.com>
2022-07-14 19:31:33 +0200

    halfWay

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [typedText, setTypedText] = useState("");
+  const [newItem, setNewItem] = useState("");
+  var itemList = [];
+
+  function handleChange(event) {
+    setTypedText(event.target.value);
+  }
+
+  function handleClick() {
+    setNewItem(typedText);
+    itemList.push(newItem);
+    console.log(itemList);
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="Next To-Do"
+          value={typedText}
+        />
+        <button type="submit" onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
           <li>A Item</li>
+          <li>{newItem}</li>
         </ul>
       </div>
     </div>
